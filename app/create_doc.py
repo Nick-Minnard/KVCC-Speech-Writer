9545e891646fc3c218d6dda3abdbbac027e4fcdf
Nick Minnard <nminnard1@gmail.com>
2022-10-07 17:08:37 -0400

    Add icons to site. Complete word document formatting.

--- app/create_doc.py
+++ app/create_doc.py
@@ -1,10 +1,20 @@
 from docx import Document
-
+from docx.shared import RGBColor, Pt
 def create_speech_doc(speech):
   d = Document()
   d.add_heading(speech.title, 0)
 
-  # for point in speech.points:
-  #   p = d.add_paragraph(point.name)
+  current_section = 1
 
+  for point in speech.data["points"]:
+    if point["section"] > current_section:
+      current_section += 1
+      r = d.add_paragraph().add_run("__________________________________________________________________________________________________________")
+      r.font.color.rgb = RGBColor(77, 168, 208)
+    p = d.add_paragraph()
+    r = p.add_run(point["name"] + ": ")
+    r.bold = True
+    r.font.size = Pt(13)
+    r = p.add_run(point["value"])
+    r.font.size = Pt(13)
   return d
--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,9 @@
 
 
 # TODO: Ready for user testing by 10-17
-# - Help window / Site info
-# - Word doc formatting
 # - Help / Info popups
 # - Backups?
 # - KVCC color styling / bootstrap icons
-# - Disclaimer...?
-# - Sticky footer with copyright
-# - Database research
-# - Home link from editor
 # - inline css to editor sheet
 # - Thorough documentation
 
